271559b5a12c2ba4d5d44e2cf63d107d456433c5
grvbrk <gouravbarikofficial@gmail.com>
2024-11-16 18:54:42 +0530

    added getHTML func

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+	if len(args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	BASE_URL := os.Args[1]
+	fmt.Printf("starting crawl of: %s", BASE_URL)
+	html, err := getHTML(BASE_URL)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(html)
+}
+
+func getHTML(rawURL string) (string, error) {
+	res, err := http.Get(rawURL)
+	if err != nil {
+		return "", err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode >= 400 {
+		return "", fmt.Errorf("status code %v", res.StatusCode)
+	}
+
+	if !strings.HasPrefix(res.Header.Get("content-type"), "text/html") {
+		return "", fmt.Errorf("content-type is not text/html")
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+
 }
